f5dea807a327bd508f02b113227335bc12ae0915
Laurent <laurentk67@hotmail.fr>
2020-10-27 22:37:34 +0100

    modified:   functional_test.py "mise en place d'une fonction pour tester la presence d'une element dans une liste"

--- functional_test.py
+++ functional_test.py
@@ -11,6 +11,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Teste de connexion sur la page d'accueil
         self.browser.get('http://localhost:8000')
@@ -23,7 +28,7 @@ class NewVisitorTest(unittest.TestCase):
         # Inviter à entrer des elements dans la todo
         inputbox = self.browser.find_element_by_id("id_new_item")
         self.assertEqual(
-            inputbox.get_attribute("placehorlder"),
+            inputbox.get_attribute("placeholder"),
             "Enter a to-do item"
         )
 
@@ -36,18 +41,23 @@ class NewVisitorTest(unittest.TestCase):
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_element_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == "1: Buy peacock feathers" for row in rows)
-        )
-
-        self.fail("Finish the test !")
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         # Le text box invite pour saisir un nouvel element
         # Elle entre "Use peacock feathers to make a fly"
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys("Use peacock feathers to make a fly")
+        inputbox.send_keys(Keys.ENTER)
 
         # La page se met à jour, elle affiche les deux elements
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_element_by_tag_name('tr')
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table(
+            '2: Use peacock feathers to make a fly')
 
         # Elle se demande si le site se souvient des ces elements sur un url specifique
+        self.fail("Finish the test !")
 
         # elle visite son url, elle affiche ces elements
 
