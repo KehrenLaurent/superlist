d919e74613ebab21c8b7bb0a7827e12e9840c2c0
Laurent <laurentk67@hotmail.fr>
2020-10-31 19:49:02 +0100

    Model for list items add associated migration

--- lists/tests.py
+++ lists/tests.py
@@ -5,6 +5,7 @@ from django.test import TestCase
 from django.http import HttpRequest
 
 from lists.views import home_page
+from lists.models import Item
 
 # Create your tests here.
 
@@ -32,3 +33,22 @@ class HomePageTest(TestCase):
             {'new_item_text': 'A new list item'}
         )
         self.assertEqual(response.content.decode(), expected_html)
+
+
+class ItemModelTest(TestCase):
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = 'The first (ever) list item'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'Item the seconde'
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(2, saved_items.count())
+
+        first_saved_item = saved_items[0]
+        seconde_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(seconde_saved_item.text, 'Item the seconde')
