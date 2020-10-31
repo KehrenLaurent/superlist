190e2b06d1a97fd86f7882a446054bc8aec6cc87
Laurent <laurentk67@hotmail.fr>
2020-10-31 21:04:56 +0100

    new file:   functional_tests/__init__.py 	new file:   functional_tests/__pycache__/__init__.cpython-37.pyc 	new file:   functional_tests/__pycache__/test.cpython-37.pyc 	renamed:    functional_test.py -> functional_tests/test.py

--- functional_tests/test.py
+++ functional_tests/test.py
@@ -1,9 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import unittest
+from django.test import LiveServerTestCase
 
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
         self.browser.implicitly_wait(3)
@@ -18,7 +18,7 @@ class NewVisitorTest(unittest.TestCase):
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Teste de connexion sur la page d'accueil
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
 
         # verifier la mention to-do dans le title de la page
         self.assertIn('To-Do', self.browser.title)
@@ -60,7 +60,3 @@ class NewVisitorTest(unittest.TestCase):
         self.fail("Finish the test !")
 
         # elle visite son url, elle affiche ces elements
-
-
-if __name__ == "__main__":
-    unittest.main(warnings='ignore')
