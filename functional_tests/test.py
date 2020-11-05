3466ebded5a0434aa2870c1678967729c1f0e5d1
Laurent <laurentk67@hotmail.fr>
2020-11-05 22:03:01 +0100

    first steps of FT for layout + styling

--- functional_tests/test.py
+++ functional_tests/test.py
@@ -86,3 +86,16 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertNotIn("make a fly", page_text)
 
         # satisfait, ils vont se coucher
+
+    def test_layout_and_styling(self):
+        # Edith arrive sur la page d'accueil
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        # Elle verifie que la zone de texte est bien alignée
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=5
+        )
