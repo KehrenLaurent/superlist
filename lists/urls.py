de93bfa56e2ca5c43797dce7193bb8da4e9f9870
Laurent <laurentk67@hotmail.fr>
2020-11-05 21:45:29 +0100

    modified:   lists/urls.py 	modified:   superlists/urls.py

--- lists/urls.py
+++ lists/urls.py
@@ -4,7 +4,6 @@ import lists.views as views
 app_name = 'lists'
 
 urlpatterns = [
-    path('', views.home_page, name='home'),
     path('lists/<int:pk>/',
          views.view_list, name='view_list'),
     path('lists/<int:pk>/add_item', views.add_item, name='add_item'),
